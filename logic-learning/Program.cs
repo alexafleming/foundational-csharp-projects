980bb27324bb81898157daf6f228dfd8b600ae58
alexafleming <alexaflemingmn@gmail.com>
2024-01-31 14:20:17 -0600

    logic-learning

---
+++ logic-learning/Program.cs
@@ -0,0 +1,31 @@
+﻿// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
+
+
+
+
+
+
