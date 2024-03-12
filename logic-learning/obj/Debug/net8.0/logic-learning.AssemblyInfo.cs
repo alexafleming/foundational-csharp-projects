43240cb04511b71e234a26e7206b1a6366a8d861
alexafleming <alexaflemingmn@gmail.com>
2024-03-12 10:30:17 -0500

    petting zoo activity

--- logic-learning/obj/Debug/net8.0/logic-learning.AssemblyInfo.cs
+++ logic-learning/obj/Debug/net8.0/logic-learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd764639e3cdd93b64389e600690e7313e3895b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46054a1cbe02818b248e832971ad60cbe0a1698")]
 [assembly: System.Reflection.AssemblyProductAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
