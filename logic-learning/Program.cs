e6e4c4225750bf19c23ef42c0935ae73c9620e75
alexafleming <alexaflemingmn@gmail.com>
2024-03-13 10:00:04 -0500

    debugging practice

--- logic-learning/Program.cs
+++ logic-learning/Program.cs
@@ -1109,37 +1109,78 @@ else ipAddress is invalid
 
 
 
- /* 
-This code uses a names array and corresponding methods to display
-greeting messages
+// string? readResult;
+// int startIndex = 0;
+// bool goodEntry = false;
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+
+// // Display the array to the console.
+// Console.Clear();
+// Console.Write("\n\rThe 'numbers' array contains: { ");
+// foreach (int number in numbers)
+// {
+//     Console.Write($"{number} ");
+// }
+
+// // To calculate a sum of array elements, 
+// //  prompt the user for the starting element number.
+// Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
+// while (goodEntry == false)
+// {
+//     readResult = Console.ReadLine();
+//     goodEntry = int.TryParse(readResult, out startIndex);
+
+//     if (startIndex > 5)
+//     {
+//         goodEntry = false;
+//         Console.WriteLine("\n\rEnter an integer value between 1 and 5");
+//     }
+// }
+
+// // Display the sum and then pause.
+// Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
+
+// Console.WriteLine("press Enter to exit");
+// readResult = Console.ReadLine();
+
+// // This method returns the sum of elements n through 5
+// static int SumValues(int[] numbers, int n)
+// {
+//     n--;
+//     int sum = 0;
+//     for (int i = n; i < numbers.Length; i++)
+//     {
+//         sum += numbers[i];
+//     }
+//     return sum;
+// }
+
+
+
+// bool exit = false;
+// var rand = new Random();
+// int num1 = 5;
+// int num2 = 5;
+
+// do
+// {
+//     num1 = rand.Next(1, 11);
+//     num2 = num1 + rand.Next(1, 51);
+
+// } while (exit == false);
+
+/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
 */
+int x = 5;
 
-string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+ChangeValue(x);
 
-string messageText = "";
+Console.WriteLine(x);
 
-foreach (string name in names)
+void ChangeValue(int value) 
 {
-    if (name == "Sophia")
-        messageText = SophiaMessage();
-    else if (name == "Andrew")
-        messageText = AndrewMessage();
-    else if (name == "AllGreetings")
-        messageText = SophiaMessage();
-        messageText = messageText + "\n\r" + AndrewMessage();
-
-    Console.WriteLine(messageText + "\n\r");
-}
-
-bool pauseCode = true;
-while (pauseCode == true);
-
-static string SophiaMessage()
-{
-    return "Hello, my name is Sophia.";
-}
-
-static string AndrewMessage()
-{
-    return "Hi, my name is Andrew. Good to meet you.";
+    value = 10;
 }
--- logic-learning/obj/Debug/net8.0/logic-learning.AssemblyInfo.cs
+++ logic-learning/obj/Debug/net8.0/logic-learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43240cb04511b71e234a26e7206b1a6366a8d861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a00d221a280062ad9830004d71a045c5afb376e")]
 [assembly: System.Reflection.AssemblyProductAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("logic-learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
